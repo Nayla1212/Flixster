805cc01a8cca41caaac3237848c19d4ec8d58a3b
Nayla1212 <nayla.boorady@gmail.com>
2021-06-25 17:31:19 -0400

    Initial commit

---
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -0,0 +1,55 @@
+package com.example.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixster.models.Movie;
+
+import org.parceler.Parcels;
+
+import adapters.GlideApp;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+    Context context;
+
+    // the movie to display
+    Movie movie;
+
+    // the view objects
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar rbVoteAverage;
+    ImageView ivPoster;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+        // resolve the view objects
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvOverview = (TextView) findViewById(R.id.tvOverview);
+        rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+        ivPoster = (ImageView) findViewById(R.id.ivPoster);
+
+        // unwrap the movie passed in via intent, using its simple name as a key
+        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
+
+        // set the title and overview
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+        Log.e("MovieDetailsActivity", "image: "+movie.getPosterPath());
+        Glide.with(this).load(movie.getPosterPath()).into(ivPoster);
+
+        // vote average is 0..10, convert to 0..5 by dividing by 2
+        float voteAverage = movie.getVoteAverage().floatValue();
+        rbVoteAverage.setRating(voteAverage / 2.0f);
+    }
+}
